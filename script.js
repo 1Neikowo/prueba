49ed04c61e3138352a8b1965e14099247963b7ce
1Neikowo <71148894+1Neikowo@users.noreply.github.com>
2025-08-01 05:15:29 -0400

    mejoras

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const imgs = ["Screenshot_2.png", "Screenshot_3.png", "Screenshot_4.png"];
+let index = 0;
 function mostrar(){
-    document.getElementById('sorpresa').innerHTML = "TE AMOOOOOOO ❤❤❤❤";
-    document.querySelector("img").src = 'Screenshot_2.png';
+    document.getElementById('sorpresa').innerHTML = "Te amo mucho ❤❤❤❤";
+     document.getElementById('sorpresa').style.opacity = "100%"
+    document.querySelector("img").src = imgs[0];
+    document.querySelector("img").style.opacity="100%"
+    document.querySelector(".botones-carrusel").style.opacity="100%";
+    document.querySelector(".a").style.height = "auto";
+}
+function toggle(){
+    index = (index+1) % imgs.length;
+    document.querySelector("img").setAttribute("src",imgs[index]);
+}
+function toggleAtras(){
+    index = (index - 1 + imgs.length) % imgs.length
+    document.querySelector("img").src = imgs[index];
 }
